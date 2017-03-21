46e79139ecc94edd149c83c0624db4309b43564c
teodorxxx <tdrzyzga@gmail.com>
2017-03-21 14:23:20 +0100

    added checkbox

---
+++ WydatkiDomoweWeb.WebUI/Controllers/CheckboxController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WydatkiDomoweWeb.Domain.Abstract;
+
+namespace WydatkiDomoweWeb.WebUI.Controllers
+{
+    public class CheckboxController : Controller
+    {
+        private IBillRepository repository;
+
+        public CheckboxController(IBillRepository billRepository)
+        {
+            this.repository = billRepository;
+        }
+
+        public PartialViewResult Checkbox()
+        {
+            IEnumerable<string> checkboxName = repository.BillNames.Select(x => x.Name).OrderByDescending(x => x);
+
+            return PartialView(checkboxName);
+        }
+
+    }
+}
