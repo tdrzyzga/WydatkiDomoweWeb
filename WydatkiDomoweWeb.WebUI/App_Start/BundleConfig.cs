438f049f522f1fa2fd246e5fe421975ff9b7c6a5
teodorxxx <tdrzyzga@gmail.com>
2017-04-05 12:21:19 +0200

    not finished

---
+++ WydatkiDomoweWeb.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace WydatkiDomoweWeb.WebUI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+        }
+    } 
+}
