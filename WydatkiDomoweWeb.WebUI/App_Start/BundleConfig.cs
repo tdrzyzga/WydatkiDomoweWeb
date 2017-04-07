0e9dcb861426b7fa8f238fdbf012e6a46a1cedd2
teodorxxx <tdrzyzga@gmail.com>
2017-04-07 20:55:38 +0200

    added datetimepicker

--- WydatkiDomoweWeb.WebUI/App_Start/BundleConfig.cs
+++ WydatkiDomoweWeb.WebUI/App_Start/BundleConfig.cs
@@ -11,6 +11,13 @@ namespace WydatkiDomoweWeb.WebUI
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/*.css"));
+            bundles.Add(new ScriptBundle("~/bundles/webscripts")
+                .Include(
+                 "~/Scripts/jquery-{version}.js",
+                "~/Scripts/moment-with-locales.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/bootstrap-datetimepicker.min.js"
+               ));
         }
     } 
 }
