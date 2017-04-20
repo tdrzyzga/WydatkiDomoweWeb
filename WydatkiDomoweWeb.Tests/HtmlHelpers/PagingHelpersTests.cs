76bb90f5ba0e9abef7f41064bd68d14cb0e6a05f
teodorxxx <tdrzyzga@gmail.com>
2017-04-20 18:52:32 +0200

    update tests

--- WydatkiDomoweWeb.Tests/HtmlHelpers/PagingHelpersTests.cs
+++ WydatkiDomoweWeb.Tests/HtmlHelpers/PagingHelpersTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using WydatkiDomoweWeb.WebUI.Models;
+using System.Web.Mvc.Ajax;
 
 namespace WydatkiDomoweWeb.WebUI.HtmlHelpers.Tests
 {
@@ -25,9 +26,9 @@ namespace WydatkiDomoweWeb.WebUI.HtmlHelpers.Tests
                 CurrentPage = 3
             };
 
-            MvcHtmlString result = myHelper.PageLinks(pagingInfo, i => "Strona" + i);
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, i => "Strona" + i, new AjaxOptions { UpdateTargetId = "homeBillTable" });
 
-            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Strona1"">1</a><a class=""btn btn-default"" href=""Strona2"">2</a><a class=""btn btn-default btn-primary selected"" href=""Strona3"">3</a>", result.ToString());
+            Assert.AreEqual(@"<a class=""btn btn-default"" data-ajax=""true"" data-ajax-mode=""replace"" data-ajax-update=""#homeBillTable"" href=""Strona1"">1</a><a class=""btn btn-default"" data-ajax=""true"" data-ajax-mode=""replace"" data-ajax-update=""#homeBillTable"" href=""Strona2"">2</a><a class=""btn btn-default btn-primary selected"" data-ajax=""true"" data-ajax-mode=""replace"" data-ajax-update=""#homeBillTable"" href=""Strona3"">3</a>", result.ToString());
 
         }
     }
