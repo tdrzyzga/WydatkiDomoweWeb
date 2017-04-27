96043ae99c20d9a239dedd00a4dbc8109d5e825b
teodorxxx <tdrzyzga@gmail.com>
2017-04-27 14:16:12 +0200

    work modified addBill

---
+++ WydatkiDomoweWeb.WebUI/Scripts/crudBill.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    $('#selectBill').change(function () {
+        var value = $('#selectBill').val();
+
+        if (value !== "") {
+            var date = $('#' + value).val();
+            date = date.split(' ');
+            date = date[0].split('-');
+            var correctDate = date[2] + "." + date[1] + "." + date[0];
+
+            $('#datetimepicker').datetimepicker({
+                locale: 'pl'
+            });
+            $('#datetimepicker').val('Data wpłaty');
+
+            $('#requiredDate').val(correctDate);
+        }
+        else {
+            $('#datetimepicker').val('Data wpłaty');
+            $('#requiredDate').val('Termin zapłaty');
+        }
+    });
+    $('#buttonAddBill').on('click', function () {
+        if ($('#addBill').is(':hidden')) {
+            $('#addBill').show();
+        }
+        else {
+            $('#addBill').hide();
+        }
+    });
+
+})
