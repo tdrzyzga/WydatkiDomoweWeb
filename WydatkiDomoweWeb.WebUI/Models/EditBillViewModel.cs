24d5bda095ea9a5f264b9c486d2f2cadf41126ab
teodorxxx <tdrzyzga@gmail.com>
2017-05-05 13:35:24 +0200

    work added EditBills

---
+++ WydatkiDomoweWeb.WebUI/Models/EditBillViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WydatkiDomoweWeb.WebUI.Models
+{
+    public class EditBillViewModel
+    {
+        public int BillId { get; set; }
+        public string BillName { get; set; }
+        public int SelectedRecipientId { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać kwotę")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać datę")]
+        public string PaymentDate { get; set; }
+
+        public string RequiredDate { get; set; }
+
+        public List<SelectListItem> Recipients { get; set; }
+    }
+}
