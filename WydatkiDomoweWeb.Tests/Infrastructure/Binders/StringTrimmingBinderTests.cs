ba9f1d6e39e8da221cb94e118021075143cc4c2e
teodorxxx <tdrzyzga@gmail.com>
2017-06-25 22:35:09 +0200

    added StringTrimmingBinderTest but it doesn't work

---
+++ WydatkiDomoweWeb.Tests/Infrastructure/Binders/StringTrimmingBinderTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WydatkiDomoweWeb.WebUI.Infrastructure.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using System.Web.Mvc;
+using System.Collections.Specialized;
+
+namespace WydatkiDomoweWeb.WebUI.Infrastructure.Binders.Tests
+{
+    [TestClass()]
+    public class StringTrimmingBinderTests
+    {
+        [TestMethod()]
+        public void BindModelTest()
+        {
+            /*Mock<ControllerContext> mockControllerContext = new Mock<ControllerContext>();
+            Mock<ValueProviderResult> valueProviderResult = new Mock<ValueProviderResult>();
+            valueProviderResult.Setup(m => m).Returns(new ValueProviderResult(null, " o   ja pier  le  ", null));
+            valueProviderResult.Setup(m => m.AttemptedValue).Returns(" o   ja pier  le  ");
+            Mock<IValueProvider> valueProvider = new Mock<IValueProvider>();
+            valueProvider.Setup(m => m.GetValue("Trim")).Returns(valueProviderResult.Object);
+            
+            ModelBindingContext bindingContext = new ModelBindingContext
+            {
+                ValueProvider = valueProvider.Object
+            };
+
+            StringTrimmingBinder trim = new StringTrimmingBinder();
+
+            string result = trim.BindModel(mockControllerContext.Object, bindingContext).ToString();
+
+            Assert.AreEqual("o ja pierdole", result);*/
+        }
+    }
+}
