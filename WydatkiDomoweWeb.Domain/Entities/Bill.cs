ee73ae74127803227bdba788ed3983504dbcd233
Tomasz Drzyzga <tomasz.drzyzga@iteo.co>
2017-09-26 21:06:43 +0200

    Test git extension

--- WydatkiDomoweWeb.Domain/Entities/Bill.cs
+++ WydatkiDomoweWeb.Domain/Entities/Bill.cs
@@ -4,7 +4,6 @@ namespace WydatkiDomoweWeb.Domain.Entities
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
 
     [Table("Expenses.Bills")]
     public partial class Bill
